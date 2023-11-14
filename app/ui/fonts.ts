8da06f881344ded92515682884cfd23a905528d6
Edgar Albalate <dev.gardo@gmail.com>
2023-11-14 21:52:04 +0100

    step-three

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Ubuntu_Mono, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const lusitana = Lusitana({ weight: ['400', '700'], subsets: ['latin'] })
+export const ubuntuMono = Ubuntu_Mono({ weight: '400', subsets: ['latin-ext'] })
