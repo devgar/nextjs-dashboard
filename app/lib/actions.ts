5becbeb5d28f056c371def33ac69307dec3c1968
Edgar Albalate <dev.gardo@gmail.com>
2023-11-15 01:22:16 +0100

    step-fourteen

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,13 +16,30 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ date: true })
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
-export async function createInvoice(formData: FormData) {
-const { id, customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+        customerId?: string[],
+        amount?: string[],
+        status?: string[]
+    },
+    message?: string | null
+  }
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     id: formData.get('id'),
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+
+  if (!validatedFields.success) {
+    return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Misssing Fields. Failed to Create Invoice',
+    }
+  }
+  const { id, customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
   
